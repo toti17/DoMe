f7f7f079eabb99691da7b528198ea1b1fd5324e8
Third <thirdcruz13@gmail.com>
2017-04-26 14:06:43 +0800

    initial layout

--- app/Home.js
+++ app/Home.js
@@ -14,6 +14,8 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './styles/homeStyles.js';
 class Home extends Component {
 	navigate(routeName){
 		this.props.navigator.push({
@@ -32,9 +34,16 @@ class Home extends Component {
 
 	render() {
 		return (	
-			<View>
-						<Text >Do Me!</Text>
-			</View>
+      <View style={styles.parent}> 
+          <View style={styles.topContainer}>
+            <View style={styles.top1}>
+              <View style={styles.header1}><Text style={styles.homeText}>DO ME !</Text></View>
+            </View>
+          </View>
+          
+          <View style={styles.bottomContainer}>
+          </View>
+      </View>
 	  	);
 	}	
 }
